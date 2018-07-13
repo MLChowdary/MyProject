f9104c68ffdeff56dd2fab782345fc1805769b60
Admin <admin@Admins-Mac.local>
2018-07-13 17:20:37 +0300

    mapview e dded and code added view controller

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("hhghgsjdfkbm")
+        let map = MapViewController()
+        print(map)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
